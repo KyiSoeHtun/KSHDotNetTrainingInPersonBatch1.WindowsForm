b250bd56a3fc44b0ea5bce7c13f574705f2d0574
KyiSoeHtun <ksh.kshtun@gmail.com>
2025-09-18 21:53:30 +0630

    Saledetail.Save

--- KSHDotNetTrainingInPersonBatch1.WindowsForm/FrmSale.cs
+++ KSHDotNetTrainingInPersonBatch1.WindowsForm/FrmSale.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -108,18 +109,100 @@ namespace KSHDotNetTrainingInPersonBatch1.WindowsForm
 
             txtTotalAmount.Text = Products.Sum(x => (x.Price * x.Quantity)).ToString();
 
+            ClearControls();
+            //cboProduct.SelectedIndex = 0;
+            //txtPrice.Clear();
+            //txtQuantity.Clear();
+
+        }
+        private void ClearControls()
+        {
             cboProduct.SelectedIndex = 0;
             txtPrice.Clear();
             txtQuantity.Clear();
-
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            //var result = FormSveRequireField();
-            //if (result == false) return;
-            //// insert into Product Table and
-            //SaveProduct();
+            int saleId = CreateSale();
+          if (saleId == 0)
+            {
+                MessageBox.Show("Sale creation failed.");
+                return;
+            }
+               bool result = CreateSaleDetail(saleId);
+            if (result == false)
+            {
+                MessageBox.Show("Sale detail creation failed.");
+                return;
+            }
+            MessageBox.Show("Saved successfully.");
+            ClearControls();
+            txtTotalAmount.Clear();
+            Products = new List<ProductDto>();
+            dgvData.DataSource = null;
+            dgvData.DataSource = Products;
+        }
+        /// <summary>
+        /// Return SaleId
+        /// </summary>
+        /// <returns></returns>
+        private int CreateSale()
+        {
+            string querySaleInsert = @"INSERT INTO [dbo].[Tbl_Sale]
+                                           ([VoucherNo]
+                                           ,[SaleDate]
+                                           ,[TotalAmount])
+                                     VALUES
+                                           (@VoucherNo
+                                           ,@SaleDate
+                                           ,@TotalAmount);
+                                            SELECT SCOPE_IDENTITY();";
+            using IDbConnection db = new SqlConnection(AppSetting.ConnectionStringBuilder.ConnectionString);
+
+            var parameters = new
+            {
+                VoucherNo = "V" + DateTime.Now.ToString("yyyyMMddHHmmss"),
+                SaleDate = DateTime.Now,
+                TotalAmount = Convert.ToDecimal(txtTotalAmount.Text.Trim())
+            };
+
+            var saleId = db.ExecuteScalar(querySaleInsert, parameters);
+            if (saleId != null && saleId != DBNull.Value)
+            {
+                return Convert.ToInt32(saleId);
+            }
+            return 0;
+
+        }
+
+        private bool CreateSaleDetail(int saleId)
+        {
+            int result = 0;
+            string querySaleDetailInsert = @"INSERT INTO [dbo].[Tbl_SaleDetail]
+                                                   ([SaleId]
+                                                   ,[ProductId]
+                                                   ,[Quantity]
+                                                   ,[Price])
+                                             VALUES
+                                                   (@SaleId
+                                                   ,@ProductId
+                                                   ,@Quantity
+                                                   ,@Price);";
+            using (IDbConnection db = new SqlConnection(AppSetting.ConnectionStringBuilder.ConnectionString))
+            {
+                foreach (ProductDto item in Products)
+                {
+                    result += db.Execute(querySaleDetailInsert, new
+                    {
+                        SaleId = saleId,
+                        ProductId = item.ProductID,
+                        Quantity = item.Quantity,
+                        Price = item.Price
+                    });
+                }
+            }
+            return result == Products.Count;
         }
     }
 }
--- README.md
+++ README.md
@@ -2,12 +2,20 @@ Mini POS
 
 SaleProd
 
-Product 
-- ProductId
-- ProductCode
-- ProductName
-- Price
-- Quantity
-- DeleteFlag
+Product
+
+* ProductId
+* ProductCode
+* ProductName
+* Price
+* Quantity
+* DeleteFlag
 
 Sale
+
+
+
+Homework links
+
+https://stackoverflow.com/questions/5228780/how-to-get-last-inserted-id
+
